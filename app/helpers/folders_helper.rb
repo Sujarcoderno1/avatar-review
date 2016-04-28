965df7fb5b660d112d64f9564569f7887379ff7b
sumit <sumit.yuvasoft131@gmail.com>
2016-04-28 14:19:21 +0530

    Minor typo issue

--- app/helpers/folders_helper.rb
+++ app/helpers/folders_helper.rb
@@ -2,7 +2,7 @@ module FoldersHelper
 # require 'RMagick'
 
   def image_file_type? file
-    return false if file.path.nil?
+    return false if file.try(:path).nil?
     ["image/jpg", "image/jpeg", "image/gif", "image/png"].include? MIME::Types.type_for(file.path).first.content_type
   end
 end
